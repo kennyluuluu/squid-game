4f845b4ec5a4dec86803614b39c070ece710df12
Kenny Luu <kennyluuluu@gmail.com>
2021-11-22 21:19:01 -0800

    Create Doll class with function to turn using GSAP

--- js/main.js
+++ js/main.js
@@ -5,46 +5,67 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0xffffff, 1);
 
 const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
+// Load a glTF resource
 const loader = new THREE.GLTFLoader();
 
-// Load a glTF resource
-loader.load(
-  // resource URL
-  "../model/scene.gltf",
-  // called when the resource is loaded
-  function (gltf) {
-    scene.add(gltf.scene);
-    gltf.scene.scale.set(0.4, 0.4, 0.4);
+class Doll {
+  constructor() {
+    loader.load(
+      // resource URL
+      "../model/scene.gltf",
+      // called when the resource is loaded
+      (gltf) => {
+        scene.add(gltf.scene);
+        gltf.scene.scale.set(0.4, 0.4, 0.4);
+        gltf.scene.position.set(0, -1, 0);
+        this.doll = gltf.scene;
 
-    gltf.animations; // Array<THREE.AnimationClip>
-    gltf.scene; // THREE.Group
-    gltf.scenes; // Array<THREE.Group>
-    gltf.cameras; // Array<THREE.Camera>
-    gltf.asset; // Object
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+      },
+      // called while loading is progressing
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened");
+      }
+    );
   }
-);
 
-camera.position.z = 5;
+  lookBackward() {
+    gsap.to(this.doll.rotation, { y: -3.15, duration: 1 });
+  }
+
+  lookForward() {
+    gsap.to(this.doll.rotation, { y: 0, duration: 1 });
+  }
+}
+
+let doll = new Doll();
+setTimeout(() => {
+  doll.lookBackward();
+}, 1000);
+setTimeout(() => {
+  doll.lookForward();
+}, 5000);
 
 function animate() {
   requestAnimationFrame(animate);
-
   renderer.render(scene, camera);
 }
 animate();
